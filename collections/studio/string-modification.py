e118a06351f1a27d07c683a6b3d5fe66e0f87099
Gray Sinclair <gray.sinclair@icloud.com>
2024-08-27 20:18:28 -0500

    completed

--- collections/studio/string-modification.py
+++ collections/studio/string-modification.py
@@ -2,24 +2,28 @@ my_string = "LaunchCode"
 
 
 # a) Use string methods to remove the first three characters from the string and add them to the end.
-""" first3 = my_string[:3]
+first3 = my_string[:3]
 leftover = my_string[3:]
 newstr = leftover + first3
 print(newstr)
 
 # Use a template literal to print the original and modified string in a descriptive phrase.
-print(f'The original string is {my_string} and the new string is {newstr}.') """
+""" print('The original string is {my_string} and the new string is {newstr}.') """
 
 # b) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
-default_input = input('How many letters should I remove from the front and add to the end?:')
+default_input = int(input('How many letters should I remove from the front and add to the end? Please choose a number within the index range: '))
 
-if default_input > len(my_string):
+if default_input < 0 or default_input > len(my_string):
     default_input = 3
+    defaultnote = "As you used an illegal input, we have set your input to 3."
 else:
-    changestr = my_string[:default_input]
+    first3 = my_string[:default_input]
+    leftover = my_string[default_input:]
+    newstr = leftover + first3
+    defaultnote = ""
 
-my_string.remove(changestr)
-
-print(my_string)
+print(newstr)
 
 # c) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+
+print('{}The original string is {} and the new string is {}.'.format(defaultnote,my_string, newstr))
