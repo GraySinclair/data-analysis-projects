a1fc1ddb62e7049761d7df79170b0a7b985c0640
Gray Sinclair <gray.sinclair@icloud.com>
2024-09-03 15:57:18 -0500

    progress: almost complete

--- functions/exercises/functions-exercises.py
+++ functions/exercises/functions-exercises.py
@@ -1,5 +1,5 @@
 # Part 1 A -- Make a Line
-def make_line(size):
+def make_line(size = 9):
     line = ''
     for i in range(size):
         line += '#'
@@ -7,7 +7,7 @@ def make_line(size):
 
 # Part 1 B -- Make a Square
 # create a function using your make_line function to code a square
-def make_square(side):
+def make_square(side = 3):
     for i in range(side):
         print(make_line(side))
 
@@ -16,30 +16,43 @@ def make_square(side):
 
 
 # Part 1 C -- Make a Rectangle
-def make_rectangle(side_a,side_b):
+def make_rectangle(side_a = 2,side_b = 3):
     for i in range(side_a):
         print(make_line(side_b))
 
-make_rectangle(3,7)
-
 
 # Part 2 A -- Make a Stairs
-
-
+def make_stairs(step_num = 9):
+    for i in range(step_num):
+        print(make_line(i+1))
 
 
 
 # Part 2 B -- Make Space-Line 
-
-
+def make_space_line(numspaces = 3, numchara = 3):
+    spaceline = ''
+    for i in range(numspaces):
+        spaceline += '_'
+    for i in range(numchara):
+        spaceline += '#'
+    for i in range(numspaces):
+        spaceline += '_'
+    print(spaceline)
 
 
 
 # Part 2 C -- Make Isosceles Triangle
+def make_triangle(height = 6):
+   triangle = ""
+   for i in range(height):
+        while i > height:
+            if triangle % 2 == 0:
+                triangle += '#'
+            else:
+                triangle += '##'
+            print(triangle)
 
-
-
-
+make_triangle()
 
 # Part 3 -- Make a Diamond
 
